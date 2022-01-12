f021a5fcea4bfce9e2afe2a83771c0917cd1964b
Abinash Pradhan <36357621+abinash134@users.noreply.github.com>
2022-01-12 21:42:04 +0530

    post request

--- Hitesh/19webrequestmethods/main.go
+++ Hitesh/19webrequestmethods/main.go
@@ -9,7 +9,8 @@ import (
 
 func main() {
 	fmt.Println("welcome to web request types")
-	PerformGetRequest()
+	PerformPostjsonRequest()
+	//PerformGetRequest()
 }
 
 func PerformGetRequest() {
@@ -28,4 +29,25 @@ func PerformGetRequest() {
 	fmt.Println(bytelen)
 	fmt.Println(responseString.String())
 	//fmt.Println(string(content))
+}
+
+func PerformPostjsonRequest() {
+	const url = "http://localhost:8000/post"
+
+	requestbody := strings.NewReader(`
+		{
+			"name":"Abinash Pradhan"
+		}
+	`)
+
+	response, _ := http.Post(url, "application/json", requestbody)
+
+	defer response.Body.Close()
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(response.Body)
+	bytelen, _ := responseString.Write(content)
+	fmt.Println(bytelen)
+	fmt.Println(responseString.String())
+
 }
