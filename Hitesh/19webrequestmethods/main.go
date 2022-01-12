4539dd71800830dd52f6b2e6f9918cde68c59539
Abinash Pradhan <36357621+abinash134@users.noreply.github.com>
2022-01-12 17:48:53 +0530

    get request

--- Hitesh/19webrequestmethods/main.go
+++ Hitesh/19webrequestmethods/main.go
@@ -1,11 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+)
 
 func main() {
 	fmt.Println("welcome to web request types")
+	PerformGetRequest()
 }
 
 func PerformGetRequest() {
-	const url = ""
+	const url = "http://localhost:8000/get"
+	responce, err := http.Get(url)
+
+	if err != nil {
+		panic(err)
+	}
+	defer responce.Body.Close()
+	fmt.Println(responce.StatusCode)
+
+	var responseString strings.Builder
+	content, _ := ioutil.ReadAll(responce.Body)
+	bytelen, _ := responseString.Write(content)
+	fmt.Println(bytelen)
+	fmt.Println(responseString.String())
+	//fmt.Println(string(content))
 }
