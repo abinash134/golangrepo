71e3d9877f0fc20f3943550c63a50a800c06f48a
abinash134 <pradhanabinash134@gmail.com>
2022-01-19 09:14:30 +0530

    json handeling

---
+++ Hitesh/22handlingjson/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type course struct {
+	Name     string `json:"courcename"`
+	Price    int
+	Plat     string   `json:"website"`
+	Password string   `json:"-"`
+	Tags     []string `json:"tags,omitempty"`
+}
+
+func main() {
+	fmt.Println("my json")
+	EncodeJson()
+}
+
+func EncodeJson() {
+
+	lcoCourses := []course{
+		{"ReactJS Bootcamp", 299, "LearnCodeOnline.in", "abc123", []string{"web-dev", "js"}},
+		{"MERN Bootcamp", 199, "LearnCodeOnline.in", "bcd123", []string{"full-stack", "js"}},
+		{"Angular Bootcamp", 299, "LearnCodeOnline.in", "hit123", nil},
+	}
+
+	//package this data as JSON data
+
+	finalJson, err := json.MarshalIndent(lcoCourses, "", "\t")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", finalJson)
+
+}
